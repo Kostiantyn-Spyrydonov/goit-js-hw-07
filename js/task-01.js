196bbbb664a9bfbe2e88362afc3effbfa1189dbf
Kostiantyn <konstantinman25@gmail.com>
2024-11-09 11:47:25 +0100

    add styles for task-01

--- js/task-01.js
+++ js/task-01.js
@@ -1,4 +1,4 @@
-const categories = document.querySelectorAll('#categories .item');
+const categories = document.querySelectorAll('#categories .categories-item');
 
 console.log;
 ('Number of categories: ${categories.length}');
