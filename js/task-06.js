e0e651ef29a068cd3eff89dee41ae06c3d4d1f5c
Kostiantyn <konstantinman25@gmail.com>
2024-11-11 19:49:26 +0100

    complete task-06 and add styling to main page

---
+++ js/task-06.js
@@ -0,0 +1,48 @@
+const controls = document.querySelector('#controls');
+const input = controls.querySelector('input');
+const createButton = controls.querySelector('[data-create]');
+const destroyButton = controls.querySelector('[data-destroy]');
+const boxes = document.querySelector('#boxes');
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function createBoxes(amount) {
+  boxes.innerHTML = '';
+
+  const elements = [];
+  let size = 30;
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    box.style.margin = '5px';
+    elements.push(box);
+    size += 10;
+  }
+
+  boxes.append(...elements);
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = '';
+}
+
+createButton.addEventListener('click', () => {
+  const amount = parseInt(input.value.trim(), 10);
+
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+  } else {
+    alert('Please enter a number between 1 and 100.');
+  }
+
+  input.value = '';
+});
+
+destroyButton.addEventListener('click', destroyBoxes);
